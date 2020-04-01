17ebacca018051b9a7ab3a49eb430c86b955a499
Mike Rousos <MikeRou@Microsoft.com>
2020-04-01 14:15:39 -0400

    Add tests for holder-of-key scenarios

--- test/System.ServiceModel.Federation.Tests/Mocks/WSTrustChannelSecurityTokenProviderWithMockChannelFactory.cs
+++ test/System.ServiceModel.Federation.Tests/Mocks/WSTrustChannelSecurityTokenProviderWithMockChannelFactory.cs
@@ -1,7 +1,9 @@
 ﻿using System.IdentityModel.Selectors;
+using System.Reflection;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Security.Tokens;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.IdentityModel.Protocols.WsTrust;
 
 namespace System.ServiceModel.Federation.Tests.Mocks
 {
@@ -23,6 +25,30 @@ namespace System.ServiceModel.Federation.Tests.Mocks
         // without actually making requests to an STS for tokens.
         protected override ChannelFactory<IRequestChannel> CreateChannelFactory(IssuedSecurityTokenParameters issuedTokenParameters) =>
             new MockRequestChannelFactory();
+
+        // Update RST to include entropy
+        public void SetRequestEntropyAndKeySize(Entropy entropy, int? keySize)
+        {
+            var request = typeof(WSTrustChannelSecurityTokenProvider)
+                .GetField("_wsTrustRequest", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(this) as WsTrustRequest;
+            if (entropy != null)
+            {
+                request.Entropy = entropy;
+            }
+            if (keySize != null)
+            {
+                request.KeySizeInBits = keySize;
+            }
+        }
+
+        public void SetResponseSettings(MockResponseSettings responseSettings)
+        {
+            var channelFactory = typeof(WSTrustChannelSecurityTokenProvider)
+                .GetField("_channelFactory", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(this) as MockRequestChannelFactory;
+            channelFactory.ResponseSettings = responseSettings;
+        }
 
         // Provide access to the (non-public) token cache so that it's possible to test sharing a cache between provider instances.
         // That scenario isn't supported by the current public API, but is intended to work in the future, so it's good to make
